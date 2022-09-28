ad28c767cc8fbc18b5d6e711787be0d8b128baeb
Vadim Kliodt <368305@niuitmo.ru>
2022-09-28 18:56:37 +0300

    init

--- src/Array.java
+++ src/Array.java
@@ -19,16 +19,16 @@ public class Array {
         double[][] array = new double[A_SIZE][X_SIZE];
         for (int i = 0; i < A_SIZE; i++) {
             for (int j = 0; j < X_SIZE; j++) {
-
                 double k = x[j];
-                double value = switch (a[i]) {
-                    case 17 ->
-                            Math.pow((0.25 * Math.pow(1 - Math.cbrt(k), 2) * (3 + Math.pow(Math.sin(k) / 4, Math.atan((k + 4) / 16)))), Math.pow(Math.PI + Math.asin((k + 4) / 16), 2));
-                    case 7, 8, 10, 11, 15, 16 ->
-                            Math.log(Math.pow((5 + Math.pow(Math.tan(k), 2)) / Math.PI, Math.pow(Math.E, Math.cos(k))));
-                    default ->
-                            0.25 / (Math.log(Math.pow(Math.E, Math.pow(Math.pow((0.5 / (1 - k)), 3) / 2, 2))));
-                };
+                double value;
+
+                if (a[i] == 17) {
+                    value = Math.pow((0.25 * Math.pow(1 - Math.cbrt(k), 2) * (3 + Math.pow(Math.sin(k) / 4, Math.atan((k + 4) / 16)))), Math.pow(Math.PI + Math.asin((k + 4) / 16), 2));
+                } else if (a[i] == 7 || a[i] == 8 || a[i] == 10 || a[i] == 11 || a[i] == 15 || a[i] == 16) {
+                    value = Math.log(Math.pow((5 + Math.pow(Math.tan(k), 2)) / Math.PI, Math.pow(Math.E, Math.cos(k))));
+                } else {
+                    value = 0.25 / (Math.log(Math.pow(Math.E, Math.pow(Math.pow((0.5 / (1 - k)), 3) / 2, 2))));
+                }
 
                 array[i][j] = value;
             }
